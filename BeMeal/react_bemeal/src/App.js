68696da55e22ceb82bfe2e18d05d416d1d2c1f75
sechoi1300 <52802440+sechoi1300@users.noreply.github.com>
2022-10-15 23:43:14 -0700

    .

--- BeMeal/react_bemeal/src/App.js
+++ BeMeal/react_bemeal/src/App.js
@@ -7,24 +7,26 @@ import Home from "./routes/Home";
 import Register from "./routes/Register";
 import Login from "./routes/Login";
 import Test from "./routes/Test";
+import "./App.css"
 
 function App() {
   return <Router>
-    <Switch>
-      <Route path="/register">
-        <Register/>
-      </Route>
-      <Route path="/test">
-        <Test/>
-      </Route>
-      <Route path="/home">
-        <Home/>
-      </Route>
-      <Route path="/">
-        <Login/>
-      </Route>
-    </Switch>
-  </Router>;
+      <Switch>
+        <Route path="/register">
+          <Register/>
+        </Route>
+        <Route path="/test">
+          <Test/>
+        </Route>
+        <Route path="/home">
+          <Home/>
+        </Route>
+        <Route path="/">
+          <Login/>
+        </Route>
+      </Switch>
+    </Router>
+  
 }
 
 export default App;
--- BeMeal/react_bemeal/src/routes/Register.js
+++ BeMeal/react_bemeal/src/routes/Register.js
@@ -16,16 +16,25 @@ function Register() {
                     alignItems: 'center',
                     justifyContent: 'center',
                     flexDirection: 'column'
+                }} onSubmit={(event) => {
+                    //console.log(event.target.username.value)
+                    //console.log(event.target.password.value)
+                    const obj = {username: event.target.username.value, password: event.target.password.value}
+                    fetch('http://localhost:8080/register', {
+                        method: 'POST',
+                        mode: 'cors',
+                        body: JSON.stringify(obj)
+                    })
                 }}>
                     <label style={{
                         marginBottom: '2vh',
                     }}>
                         <text>Enter Username: </text>
-                        <input type="text"/>
+                        <input type="text" name="username"/>
                     </label>
                     <label>
                         <text>Enter Password: </text>
-                        <input type="text" />
+                        <input type="text" name="password"/>
                     </label>
                     <button>Submit</button>
                 </form>
